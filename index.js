d583dc6e7a0df041400ee4ca4800d63da00d4ffb
Oliver Haack <zccoliver@gmail.com>
2018-01-14 19:05:56 +0100

    Mongoose Part 3

--- index.js
+++ index.js
@@ -8,35 +8,54 @@ const connect = mongoose.connect(url, {
     useMongoClient: true
 });
 
+/*
 connect.then((db) => {
 
+    // A dropping of the collection may be necessary if an 'E1000 key duplicate error' is present on npm start
+    return db.collection('dishes').drop();
+
+});
+*/
+
+connect.then((db) => {
+    
     console.log('Connected correctly to server');
 
     Dishes.create({
-        name: 'Uthapizza',
-        description: 'Test'
+        name: 'Uthappizza',
+        description: 'test'
     })
     .then((dish) => {
         console.log(dish);
-        
+
         return Dishes.findByIdAndUpdate(dish._id, {
-                $set: {
-                    description: 'Updated Test'
-                }
-            }, {
-                new: true
-            })
-            .exec();
+            $set: { description: 'Updated test'}
+        },{ 
+            new: true 
+        })
+        .exec();
     })
     .then((dish) => {
         console.log(dish);
+
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m getting a sinking feeling!',
+            author: 'Leonardo di Carpaccio'
+        });
+
+        return dish.save();
+    })
+    .then((dish) => {
+        console.log(dish);
+
         return db.collection('dishes').drop();
     })
     .then(() => {
-        return db.close();        
+        return db.close();
     })
     .catch((err) => {
-
+        console.log(err);
     });
 
 });
